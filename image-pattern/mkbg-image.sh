765177f294ade8887912d73ff5f6e62442e7b111
Tim Urista <t_urista@apple.com>
2023-03-25 18:08:24 -0700

    fix size

--- image-pattern/mkbg-image.sh
+++ image-pattern/mkbg-image.sh
@@ -5,7 +5,7 @@ python3 bg-image-maker.py \
     "/Users/admintim/Documents/tile-patterns/flowers-out/output_10_images/bg.png" \
     "Floral Fantasy Starter" \
     "Enchanting 10-Piece Pattern Collection" \
-    --num_images 7 \
+    --num_images 6 \
     --title_font "/Users/admintim/Library/Fonts/BunchBlossomsPersonalUse-0nA4.ttf" \
     --tagline_font "/Users/admintim/Library/Fonts/IndieFlower-Regular.ttf" \
 
@@ -14,7 +14,7 @@ python3 bg-image-maker.py \
     "/Users/admintim/Documents/tile-patterns/flowers-out/output_20_images/bg.png" \
     "Floral Fantasy Value" \
     "Enchanting 20-Piece Pattern Collection" \
-    --num_images 13 \
+    --num_images 12 \
     --title_font "/Users/admintim/Library/Fonts/BunchBlossomsPersonalUse-0nA4.ttf" \
     --tagline_font "/Users/admintim/Library/Fonts/IndieFlower-Regular.ttf" \
 
@@ -23,6 +23,6 @@ python3 bg-image-maker.py \
     "/Users/admintim/Documents/tile-patterns/flowers-out/output_50_images/bg.png" \
     "Floral Fantasy Epic" \
     "Enchanting 50-Piece Pattern Collection" \
-    --num_images 19 \
+    --num_images 18 \
     --title_font "/Users/admintim/Library/Fonts/BunchBlossomsPersonalUse-0nA4.ttf" \
     --tagline_font "/Users/admintim/Library/Fonts/IndieFlower-Regular.ttf" \
